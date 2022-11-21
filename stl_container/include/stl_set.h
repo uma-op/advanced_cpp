6750c6651b06d30109c319aae974f2fee7d1da46
uma-op <bannikovtop@gmail.com>
2022-11-21 14:55:09 +0300

    Рабочее добавление и удаление

--- stl_container/include/stl_set.h
+++ stl_container/include/stl_set.h
@@ -5,60 +5,210 @@
 
 template<typename Key, typename Compare = std::less<Key>, typename Allocator = std::allocator<Key>>
 class Set {
-  private:
+  public:
     struct ListedAVLTree {
         ListedAVLTree *left, *right, *prev, *next;
 		int height;
         Key value;
 
-		ListedAVLTree(Key _value) : value(_value);
+		ListedAVLTree(Key _value) : left(nullptr), right(nullptr), prev(nullptr), next(nullptr), height(1), value(_value) {};
     };
 
     ListedAVLTree *t;
 	ListedAVLTree *begin, *end;
+	Compare cmp;
 
-	ListedAVLTree* rotate_left(ListedAVLTree *n);
-	ListedAVLTree* rotate_right(ListedAVLTree *n);
-	ListedAVLTree* insert_(ListedAVLTree *t, ListedAVLTree *n);
-	ListedAVLTree* erase_(ListedAVLTree *t, Key val);
+	ListedAVLTree* rotate_left(ListedAVLTree *n) {
+		if (n->right == nullptr) return n;
 
-	ListedAVLTree* balance(ListedAVLTree *n);
-	int bfactor(ListedAVLTree *n);
-	ListedAVLTree* rm(ListedAVLTree *n);
-	ListedAVLTree* min(ListedAVLTree *n);
-	void normalize(ListedAVLTree *n);
+		ListedAVLTree *p = n->right;
+		n->right = p->left;
+		p->left = n;
 
-  public:
+		this->normalize(n);
+		this->normalize(p);
+
+		return p;
+	}
+
+	ListedAVLTree* rotate_right(ListedAVLTree *n) {
+		if (n->left == nullptr) return n;
+
+		ListedAVLTree *p = n->left;
+		n->left = p->right;
+		p->right = n;
+
+		this->normalize(n);
+		this->normalize(p);
+		
+		return p;
+	}
+	ListedAVLTree* insert_(ListedAVLTree *t, ListedAVLTree *n, ListedAVLTree *b) {
+		if (t == nullptr) {
+			if (b == nullptr) {
+				n->next = this->begin;
+				this->begin = n;
+				
+				if (n->next != nullptr) n->next->prev = n;
+			} else {
+				n->prev = b;
+				n->next = b->next;
+				b->next = n;
+				
+				if (n->next != nullptr) n->next->prev = n;
+			}
+			return n;
+		}
+
+		if(this->cmp(n->value, t->value)) t->left = insert_(t->left, n, t);
+		else t->right = insert_(t->right, n, b);
+
+		return this->balance(t);
+	}
+
+	ListedAVLTree* erase_(ListedAVLTree *t, Key val) {
+		if (t == nullptr) return nullptr;
+		if (this->cmp(val, t->value)) {
+			t->left = this->erase_(t->left, val);
+		} else if (this->cmp(t->value, val)) {
+			t->right = this->erase_(t->right, val);
+		} else {
+			if (t->prev != nullptr) t->prev->next = t->next;
+			else {
+				this->begin = t->next;
+				this->begin->prev = nullptr;
+			}
+
+			if (t->next != nullptr) t->next->prev = t->prev;
+			else {
+				this->end = t->prev;
+				this->end->next = nullptr;
+			}
+
+			ListedAVLTree* q = t->left;
+			ListedAVLTree* r = t->right;
+
+			if ( r == nullptr ) return q;
+
+			ListedAVLTree* m = this->min(r);
+			m->right = this->rm(r);
+			m->left = q;
+			return this->balance(m);
+		}
+
+		return this->balance(t);
+	}
+
+	ListedAVLTree* balance(ListedAVLTree *n) {
+		this->normalize(n);
+
+		if(this->bfactor(n) == 2) {
+			if(this->bfactor(n->right) < 0) n->right = this->rotate_right(n->right);
+			return this->rotate_left(n);
+		}
+		if (this->bfactor(n) == -2) {
+			if(this->bfactor(n->left) > 0) n->left = this->rotate_left(n->left);
+			return this->rotate_right(n);
+		}
+
+		return n;
+	}
+
+	int bfactor(ListedAVLTree *n) {
+		int lh, rh;
+
+		if (n->left == nullptr) lh = 0;
+		else lh = n->left->height;
+
+		if (n->right == nullptr) rh = 0;
+		else rh = n->right->height;
+
+		return rh - lh;
+	}
+
+	ListedAVLTree* rm(ListedAVLTree *n) {
+		if (n->left == nullptr) return n->right;
+		n->left = this->rm(n->left);
+		return this->balance(n);
+	}
+
+	ListedAVLTree* min(ListedAVLTree *n) {
+		while (n->left) n = n->left;
+    	return n;
+	}
+
+	void normalize(ListedAVLTree *n) {
+		int lh, rh;
+
+		if (n->left == nullptr) lh = 0;
+		else lh = n->left->height;
+
+		if (n->right == nullptr) rh = 0;
+		else rh = n->right->height;
+
+		n->height = (lh > rh ? lh : rh) + 1;
+	}
+
+	// public:
 
     class iterator : public std::iterator<std::bidirectional_iterator_tag, ListedAVLTree> {
-		bool operator!=(iterator other);
-		bool operator==(iterator other);
+	public:
+		ListedAVLTree *value;
 
-		Key operator* ();
+		iterator(ListedAVLTree *_value) : value(_value) {}
 
-		iterator operator++();
-		iterator operator++(iterator);
-		iterator operator--();
-		iterator operator--(iterator);
+		bool operator!=(iterator other) {
+			return this->value != other.value;
+		}
+
+		bool operator==(iterator other) {
+			return this->value == other.value;
+		}
+
+		Key operator* () {
+			return this->value->value;
+		}
+
+		iterator operator++(int) {
+			this->value = value->next;
+			return *this;
+		}
+
+		// iterator operator++(iterator) {};
+		iterator operator--(int) {
+			this->value = value->prev;
+			return this;
+		}
+		// iterator operator--(iterator);
 	};
 
-    Set();
+    Set() : t(nullptr), begin(nullptr), end(nullptr), cmp() {}
 
-    template<typename InputIt>
-    Set(InputIt begin, InputIt end);
+    // template<typename InputIt>
+    // Set(InputIt begin, InputIt end);
 
-    Set(std::initializer_list<Key> init);
-    Set(const Set &other);
-    Set& operator=(const Set &other);
-    ~Set();
+    // Set(std::initializer_list<Key> init);
+    // Set(const Set &other);
+    // Set& operator=(const Set &other);
+    ~Set() = default;
 
-    iterator begin() const;
-    iterator end() const;
+    iterator cbegin() const {
+		return iterator(this->begin);
+	}
+
+    iterator cend() const {
+		return iterator(this->end);
+	}
+
 	iterator find(Key val) const;
 	iterator lower_bound(Key val) const;
 	
-	void insert(Key val);
-	void erase(Key val);
-};
+	void insert(Key val) {
+		ListedAVLTree *n = new ListedAVLTree(val);
+    	this->t = this->insert_(this->t, n, nullptr);
+	}
 
-#include "impl.cpp"
+	void erase(Key val) {
+		this->erase_(this->t, val);
+	}
+};
--- stl_container/tests/test.cpp
+++ stl_container/tests/test.cpp
@@ -5,8 +5,38 @@ class BaseSuite : public ::testing::Test {
   protected:
     void SetUp() {};
     void TearDown() {};
+
+  	Set<int> s;
 };
 
 TEST_F(BaseSuite, passed) {
-    EXPECT_EQ(42, 42);
+    s.insert(10);
+    // std::cout << s.t->value << std::endl;
+    EXPECT_EQ(s.t->value, 10);
+    s.insert(11);
+    EXPECT_EQ(s.t->right->value, 11);
+    s.insert(7);
+    EXPECT_EQ(s.t->left->value, 7);
+    s.insert(8);
+    EXPECT_EQ(s.t->left->right->value, 8);
+    s.insert(9);
+    EXPECT_EQ(s.t->left->value, 8);
+    EXPECT_EQ(s.t->left->left->value, 7);
+    EXPECT_EQ(s.t->left->right->value, 9);
+
+	s.erase(7);
+	EXPECT_EQ(s.t->left->left, nullptr);
+
+	s.erase(8);
+	EXPECT_EQ(s.t->left->value, 9);
+
+	s.insert(7);
+
+	auto b = s.cbegin();
+	auto e = s.cend();
+
+	do {
+		std::cout << *b << std::endl;
+		b++;
+	} while (b != e);
 }
